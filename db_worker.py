1a28e350504e67d72079028078566726e7e36c6a
Solodovnikova Ekaterina <colo18@yandex.ru>
2018-12-27 15:29:34 +0500

    fixes

--- db_worker.py
+++ db_worker.py
@@ -14,34 +14,41 @@ def disconnectDB(connection):
     print("EXIT")
     connection.close()
 
-def insert(connection, data):
-    # print(data)
+def insert(connection, data, db):
     with connection.cursor() as cursor:
-        sql = "insert into CardPayments (card_num, expire_date, cvc, amount, comments, email, notSafe)" \
-              "values (%s,%s,%s,%s,%s,%s,%s)"
-        cursor.execute(sql,
+
+        if db == 'CardPayments':
+            sql = "insert into CardPayments (card_num, expire_date, cvc, amount, comments, email)" \
+              "values (%s,%s,%s,%s,%s,%s)"
+            cursor.execute(sql,
                        (data['cardNumber'], data['expireDate'], data['cvc'],
-                        data['amount'], data['comments'], data['email'],
-                        data['notSafe']))
+                        data['amount'], data['comments'], data['email']))
+        elif db == 'RequirePayments':
+            sql = "insert into RequirePayments (name, bik, account_num, nds, amount, tel_number, email)" \
+                  "values (%s,%s,%s,%s,%s,%s,%s)"
+            cursor.execute(sql,
+                           (data['name'], data['bik'], data['numAccount'],
+                            data['nds'], data['amount'], data['tel_number'], data['email']))
     connection.commit()
 
-def selectAll(connection):
+def selectAll(connection, db):
     with connection.cursor() as cursor:
-        sql = "select * from CardPayments"
+        sql = "select * from "+db
+        print (sql)
         cursor.execute(sql)
 
         return cursor.fetchall()
 
-def orderby(connection, field, order):
+def orderby(connection, field, order, db):
     with connection.cursor() as cursor:
-        sql = "select * from CardPayments order by " + field + " " + order
-
+        sql = "select * from "+ db +" order by " + field + " " + order
+        print(sql)
         cursor.execute(sql)
         return cursor.fetchall()
 
-def filter(connection, field, filter):
+def filter(connection, field, filter,db):
     with connection.cursor() as cursor:
-        sql = "select * from CardPayments " \
+        sql = "select * from "+ db +" " \
               "where "+ field +" like '%"+filter+"%'"
         print(sql)
         cursor.execute(sql)
@@ -51,11 +58,10 @@ def alterTrust(connection, index):
     with connection.cursor() as cursor:
         sql = "select notSafe from CardPayments where id="+str(index)
         cursor.execute(sql)
-        result = ''
-        if (cursor.fetchone()['notSafe'] == '0'):
-            result = '1'
+        if cursor.fetchone()['notSafe'] == 0:
+            result = 1
         else:
-            result = '0'
-        sql = "update CardPayments set notSafe="+result+" where id="+str(index)
+            result = 0
+        sql = "update CardPayments set notSafe="+str(result)+" where id="+str(index)
         cursor.execute(sql)
     connection.commit()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import db_worker
+import pdf_worker
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response, send_file
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,54 +11,59 @@ app.run()
 connection = None
 
 
-def sortPayments(sort, field_option, filter_field):
-    print(sort, field_option, filter_field)
+def sortPayments(sort, field_option, filter_field, db):
+    # print(sort, field_option, filter_field)
     if field_option == 'no':
-        return db_worker.selectAll(connection)
+        return db_worker.selectAll(connection, db)
     elif filter_field != '':
-        return db_worker.filter(connection, field_option, filter_field)
+        return db_worker.filter(connection, field_option, filter_field, db)
     else:
-        return db_worker.orderby(connection, field_option, sort)
+        return db_worker.orderby(connection, field_option, sort, db)
 
 
 @app.route('/card-payment', methods=['POST', 'GET', 'PATCH'])
 def login():
     if request.method == 'POST':
         jsonData = request.get_json()
-        db_worker.insert(connection, jsonData)
+        db_worker.insert(connection, jsonData, 'CardPayments')
         return jsonify(), 201
     elif request.method == 'GET':
         sort_option = request.args.get('sort', default='asc', type=str).upper()
         field_option = request.args.get('field', default='no', type=str)
         filter_option = request.args.get('filter', default='', type=str)
-        return jsonify(sortPayments(sort_option, field_option, filter_option))
+        return jsonify(sortPayments(sort_option, field_option, filter_option, 'CardPayments'))
     elif request.method == 'PATCH':
         changes = request.get_json()
         db_worker.alterTrust(connection, changes['changes'])
         return jsonify(), 204
 
-#
-# @app.route('/require-payment', methods=['POST', 'GET'])
-# def require():
-#     if request.method == 'POST':
-#         jsonData = request.get_json()
-#         requirePayments.append(
-#             {
-#                 'name': jsonData['name'],
-#                 'bik': jsonData['bik'],
-#                 'account_num': jsonData['account_num'],
-#                 'nds': jsonData['nds'],
-#                 'amount': jsonData['amount'],
-#                 'tel_number': jsonData['tel_number'],
-#                 'email':jsonData['email']
-#             }
-#         )
-#         print (requirePayments)
-#         return jsonify(), 201
-#     elif request.method == 'GET':
-#         return jsonify(requirePayments), 200
-#
 
+@app.route('/require-payment', methods=['POST', 'GET'])
+def require():
+    if request.method == 'POST':
+        jsonData = request.get_json()
+        db_worker.insert(connection, jsonData, 'RequirePayments')
+        return jsonify(), 201
+    elif request.method == 'GET':
+        sort_option = request.args.get('sort', default='asc', type=str).upper()
+        field_option = request.args.get('field', default='no', type=str)
+        filter_option = request.args.get('filter', default='', type=str)
+        return jsonify(sortPayments(sort_option, field_option, filter_option, 'RequirePayments'))
+
+@app.route('/create-payment', methods=['POST','GET'])
+def document():
+    if request.method == 'POST':
+        jsonData = request.get_json()
+        # print (jsonData)
+        name = pdf_worker.create_payment_document(jsonData)
+        return jsonify(name), 201
+    elif request.method == 'GET':
+        name = request.args.get('name', default='', type=str)
+        response = send_file(name, 'application/pdf')
+        response.headers['Content-Disposition'] = "attachment; filename='payment.pdf'"
+        return response
+
+#
 if __name__ == '__main__':
     try:
         connection = db_worker.connectDB()
